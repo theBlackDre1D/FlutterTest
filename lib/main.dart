41ed9d750eb40ea1e86c5864245819a894090f75
Michal Gáborík <michal.gaborik.77@gmail.com>
2018-11-19 19:36:14 +0100

    rework main.dart to separeted files - more readeble and maintable

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
+import './products_manager.dart';
 main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
-  }
+class MyApp extends StatelessWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _MyAppState();
+//   }
+// }
 
-}
+// class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,25 +20,8 @@ class MyApp extends StatefulWidget {
           appBar: AppBar(
             title: Text("Easy list"),
           ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  child: Text('Add product'),
-                  onPressed: () {}, // empty function for now
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/burger.jpg'),
-                    Text("Some text")
-                  ],
-                ),
-              ),
-            ],
-          )),
+          body: ProductManager()
+          ),
     );
   }
 }
